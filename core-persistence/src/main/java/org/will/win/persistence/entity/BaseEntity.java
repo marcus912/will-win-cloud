4416afb0989001980047b93e5bcde5b613ffa794
Marcus Lin <marcus.lin@belkin.com>
2022-03-05 23:10:29 +0800

    Add BaseEntity and auditing feature.

---
+++ core-persistence/src/main/java/org/will/win/persistence/entity/BaseEntity.java
@@ -0,0 +1,85 @@
+package org.will.win.persistence.entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity implements Serializable {
+
+  @CreationTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "created_at")
+  protected Timestamp createdAt;
+
+  @CreatedBy
+  @Column(name = "created_by")
+  protected String createdBy;
+
+  @UpdateTimestamp
+  @Temporal(TemporalType.TIMESTAMP)
+  @Column(name = "modified_at")
+  protected Timestamp modifiedAt;
+
+  @LastModifiedBy
+  @Column(name = "updated_by")
+  protected String updatedBy;
+
+  public Timestamp getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Timestamp createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public Timestamp getModifiedAt() {
+    return modifiedAt;
+  }
+
+  public void setModifiedAt(Timestamp modifiedAt) {
+    this.modifiedAt = modifiedAt;
+  }
+
+  public String getUpdatedBy() {
+    return updatedBy;
+  }
+
+  public void setUpdatedBy(String modifiedBy) {
+    this.updatedBy = modifiedBy;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    BaseEntity that = (BaseEntity) o;
+    return Objects.equals(createdAt, that.createdAt) && Objects.equals(createdBy, that.createdBy)
+      && Objects.equals(modifiedAt, that.modifiedAt) && Objects.equals(updatedBy, that.updatedBy);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(createdAt, createdBy, modifiedAt, updatedBy);
+  }
+}
