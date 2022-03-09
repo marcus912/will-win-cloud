79598c2c8981889588c97fda0bd7f4ed45e40cb5
Marcus Lin <marcus.lin@belkin.com>
2022-03-09 12:42:57 +0800

    Set status columns not nullable

--- db-migration/src/main/resources/migrations/accounts_schema/V20220303.2237__create_table_item.sql
+++ db-migration/src/main/resources/migrations/accounts_schema/V20220303.2237__create_table_item.sql
@@ -2,10 +2,10 @@ CREATE TABLE item
 (
   id          INT AUTO_INCREMENT PRIMARY KEY,
   name        VARCHAR(255) NOT NULL,
-  status      TINYINT      DEFAULT 1,
+  status      TINYINT      NOT NULL DEFAULT 1,
   comment     TEXT,
-  created_at  TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  modified_at TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  created_by  VARCHAR(256) DEFAULT NULL,
-  updated_by  VARCHAR(256) DEFAULT NULL
+  created_at  TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+  created_by  VARCHAR(256)          DEFAULT NULL,
+  updated_by  VARCHAR(256)          DEFAULT NULL
 ) ENGINE = INNODB DEFAULT CHARSET=utf8mb4 COLLATE =utf8mb4_unicode_520_ci;
--- db-migration/src/main/resources/migrations/accounts_schema/V20220304.0138__create_table_customer.sql
+++ db-migration/src/main/resources/migrations/accounts_schema/V20220304.0138__create_table_customer.sql
@@ -9,9 +9,9 @@ CREATE TABLE customer
   contact_phone VARCHAR(255),
   contact_email VARCHAR(255),
   comment       TEXT,
-  status        TINYINT      DEFAULT 1,
-  created_at    TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  modified_at   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  created_by    VARCHAR(256) DEFAULT NULL,
-  updated_by    VARCHAR(256) DEFAULT NULL
+  status        TINYINT      NOT NULL DEFAULT 1,
+  created_at    TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+  modified_at   TIMESTAMP             DEFAULT CURRENT_TIMESTAMP,
+  created_by    VARCHAR(256)          DEFAULT NULL,
+  updated_by    VARCHAR(256)          DEFAULT NULL
 ) ENGINE = INNODB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_520_ci;
