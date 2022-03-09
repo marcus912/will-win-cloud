5d30dd3c8d21707b88d0f6430c1cb45f929eeab9
Marcus Lin <marcus.lin@belkin.com>
2022-03-09 10:51:38 +0800

    Add @NoRepositoryBean to avoid creating bean with BaseRepository.

--- core-persistence/src/main/java/org/will/win/persistence/repository/BaseRepository.java
+++ core-persistence/src/main/java/org/will/win/persistence/repository/BaseRepository.java
@@ -3,12 +3,12 @@ package org.will.win.persistence.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 import org.will.win.persistence.entity.BaseEntity;
 
 import java.io.Serializable;
 
-public interface BaseRepository<T extends BaseEntity, Integer extends Serializable>
-  extends JpaRepository<T, Integer>,
-  JpaSpecificationExecutor<T>,
-  CrudRepository<T, Integer> {
+@NoRepositoryBean
+public interface BaseRepository<T extends BaseEntity, ID extends Serializable>
+  extends JpaRepository<T, ID>, JpaSpecificationExecutor<T>, CrudRepository<T, ID> {
 }
