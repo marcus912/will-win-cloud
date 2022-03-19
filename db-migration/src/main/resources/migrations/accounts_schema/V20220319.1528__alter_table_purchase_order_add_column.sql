0c86e4616ab3e0ea261db4feaad2dda1f654d83d
Marcus Lin <marcus.lin@belkin.com>
2022-03-19 15:37:47 +0800

    Update column name

--- db-migration/src/main/resources/migrations/accounts_schema/V20220319.1528__alter_table_purchase_order_add_column.sql
+++ db-migration/src/main/resources/migrations/accounts_schema/V20220319.1528__alter_table_purchase_order_add_column.sql
@@ -1,2 +1,2 @@
 ALTER TABLE purchase_order
-  ADD COLUMN `order_number` varchar(60) COLLATE utf8mb4_unicode_520_ci NOT NULL after `id`;
+  ADD COLUMN `purchase_order_number` varchar(60) COLLATE utf8mb4_unicode_520_ci NOT NULL after `id`;
